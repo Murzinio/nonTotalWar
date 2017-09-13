2346d33ca1d9f7f76e57c25b273640c85524903f
Murzinio <k.pawelczykbsh@gmail.com>
2017-09-13 17:23:02 +0200

    Displaying debug information in UnitStatsBar.

--- totalWar/headers/UnitStatsBar.h
+++ totalWar/headers/UnitStatsBar.h
@@ -12,10 +12,21 @@ namespace nonTotalWar
         Graphics& m_graphics;
         std::map<std::string, std::shared_ptr<Unit >>& m_selectedUnits;
         const SDL_Point m_size{ settings::WINDOW_WIDTH / 8, settings::WINDOW_HEIGHT / 4};
+
+        std::map<std::string, std::string> m_textFields
+        { { "State", ""},
+          { "Soldiers", "" }
+        };
+        std::map<std::string, std::string> m_debugTextFields;
+
+        void DrawTextFields();
+        void DrawDebugTextFields();
     public:
         UnitStatsBar(Graphics& graphics, 
             std::map<std::string, std::shared_ptr<Unit>>& selectedUnits);
         void Draw();
+        
+        void AddDebugTextField(std::string name, std::string text);
     };
 }
 
--- totalWar/src/UnitStatsBar.cpp
+++ totalWar/src/UnitStatsBar.cpp
@@ -15,22 +15,73 @@ void UnitStatsBar::Draw()
     SDL_Rect dstRect{ 0, m_size.y * 3, m_size.x, m_size.y};
     m_graphics.AddToQueue("ui\\unitStatsBar", srcRect, dstRect, 0, { 0, 0 }, SDL_FLIP_NONE);
 
-    SDL_Point position = { 0, dstRect.y };
+    SDL_Point position{ 0, dstRect.y };
     position.y += dstRect.h / 10;
 
     auto unit = m_selectedUnits.begin()->second;
     if (unit->GetToDestroy())
         return;
 
+    auto& stateField = m_textFields.find("State")->second;
     auto tasks = unit->GetTasks();
     if (tasks.size() == 0 && !unit->GetIsFighting())
-        m_graphics.AddTextToQueue(position, "State: Idle", { 255, 0, 0 }, 14);
-    else if(unit->GetIsFighting())
-        m_graphics.AddTextToQueue(position, "State: Fighting", { 255, 0, 0 }, 14);
+        stateField = "Idle";
+    else if (unit->GetIsFighting())
+        stateField = "Fighting";
     else
-        m_graphics.AddTextToQueue(position, "State: Moving", { 255, 0, 0 }, 14);
+        stateField = "Moving";
 
     position.y += dstRect.h / 8;
-    m_graphics.AddTextToQueue(position, "Soldiers: " + std::to_string(unit->GetSoldiers()), { 255, 0, 0 }, 14);
+    auto& soldiersField = m_textFields.find("Soldiers")->second;
+    soldiersField = std::to_string(unit->GetSoldiers());
 
+    auto angle = unit->GetAngle();
+    auto anglec = std::to_string(angle);
+
+    m_debugTextFields.clear();
+    auto unitPosition = unit->GetPosition();
+    AddDebugTextField("Position x", std::to_string(unitPosition.x));
+    AddDebugTextField("Position y", std::to_string(unitPosition.y));
+    AddDebugTextField("Angle", std::to_string(unit->GetAngle()));
+    DrawTextFields();
+    DrawDebugTextFields();
+}
+
+void UnitStatsBar::AddDebugTextField(std::string name, std::string text)
+{
+    m_debugTextFields.insert({ name, text });
+}
+
+void UnitStatsBar::DrawTextFields()
+{
+    SDL_Point position{ 0, m_size.y * 3 };
+    bool first = true;
+    for (auto field : m_textFields)
+    {
+        if (first)
+        {
+            m_graphics.AddTextToQueue(position, field.first + ": " + field.second, { 255, 0, 0 }, 14);
+            first = false;
+        }
+        else
+        {
+            position.y += 14;
+            m_graphics.AddTextToQueue(position, field.first + ": " + field.second, { 255, 0, 0 }, 14);
+        }
+    }
+}
+
+void UnitStatsBar::DrawDebugTextFields()
+{
+#ifdef _DEBUG
+    SDL_Point position{ 0, (m_size.y * 3) + static_cast<int>((m_textFields.size() + 1) * 14) };
+
+    m_graphics.AddTextToQueue(position, "DEBUG", { 255, 0, 0 }, 14);
+
+    for (auto & field : m_debugTextFields)
+    {
+        position.y += 14;
+        m_graphics.AddTextToQueue(position, field.first + ": " + field.second, { 255, 0, 0 }, 14);
+    }
+#endif
 }
